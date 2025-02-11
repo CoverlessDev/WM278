7ec2f375e888aec03813794e739bdcdf9e04f5eb
Max <max.h.goodman@gmail.com>
2025-02-11 22:50:28 +0000

    #1 Base Commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from flask.views import MethodView
+
+app = Flask(__name__)
+
+# Sample inventory data
+inventory = [
+    #this will be handled with a JSON
+]
+
+class HomePage(MethodView):
+    def get(self):
+        # Render the main landing page with inventory information
+        return render_template('index.html', inventory=inventory)
+
+class InventoryPage(MethodView):
+    def get(self):
+        # Render the inventory tracking page
+        return render_template('inventory.html', inventory=inventory)
+
+class PurchasePage(MethodView):
+    def get(self):
+        # Render the purchasing page
+        return render_template('purchase.html')
+
+class ManagementPage(MethodView):
+    def get(self):
+        # Render the management page for authorised users
+        return render_template('management.html')
+
+# URL route mappings using as_view to register the class-based views
+app.add_url_rule('/', view_func=HomePage.as_view('home'))
+app.add_url_rule('/inventory', view_func=InventoryPage.as_view('inventory'))
+app.add_url_rule('/purchase', view_func=PurchasePage.as_view('purchase'))
+app.add_url_rule('/management', view_func=ManagementPage.as_view('management'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
